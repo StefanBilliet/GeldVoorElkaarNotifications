6d249a367bf29159414521f0d55dc7c0fed1c9e2
sbillietcsc <sbilliet@csc.com>
2014-12-15 15:02:00 +0100

    Parsed Graydon rating

--- ProjectRecordParser.js
+++ ProjectRecordParser.js
@@ -17,12 +17,14 @@ module.exports = function ProjectRecordParser () {
       var projectName = htmlNode.find("#ctl00_ctl00_ctl00_ContentPlaceHolderDefault_MainContentPlaceHolder_ctl00_ProjectenFilter_4_Projectgegevens1_LeningRepeater_ctl" + projectIndex + "_ProjectInformatie1_ProjectNaamLabel").first().text();
       var loanGoal = htmlNode.find("#ctl00_ctl00_ctl00_ContentPlaceHolderDefault_MainContentPlaceHolder_ctl00_ProjectenFilter_4_Projectgegevens1_LeningRepeater_ctl" + projectIndex + "_ProjectInformatie1_LeendoelLabel").first().text();
       var classification = htmlNode.find("#ctl00_ctl00_ctl00_ContentPlaceHolderDefault_MainContentPlaceHolder_ctl00_ProjectenFilter_4_Projectgegevens1_LeningRepeater_ctl" + projectIndex + "_ProjectInformatie1_ClassificatieLabel").first().text();
+      var graydonRating = htmlNode.find("#ctl00_ctl00_ctl00_ContentPlaceHolderDefault_MainContentPlaceHolder_ctl00_ProjectenFilter_4_Projectgegevens1_LeningRepeater_ctl" + projectIndex + "_ProjectInformatie1_GraydonRatingLabel").first().text();
 
       debugger;
       return {
         projectName : projectName,
         loanGoal : loanGoal,
-        classification : classification
+        classification : classification,
+        graydonRating : graydonRating
       };
     });
 
