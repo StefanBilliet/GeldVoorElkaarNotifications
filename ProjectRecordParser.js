064ebbb8be05c2b808141d59cfa08b869201a244
sbillietcsc <sbilliet@csc.com>
2014-12-15 14:56:49 +0100

    Parsed loan goal

--- ProjectRecordParser.js
+++ ProjectRecordParser.js
@@ -12,9 +12,15 @@ module.exports = function ProjectRecordParser () {
       var oneBasedIndex = zeroBasedIndex + 1;
       var projectIndex = zeroBasedIndex.toString().length === 1 ? '0' + oneBasedIndex : oneBasedIndex;
 
-      var projectName = $(projectNode).find("#ctl00_ctl00_ctl00_ContentPlaceHolderDefault_MainContentPlaceHolder_ctl00_ProjectenFilter_4_Projectgegevens1_LeningRepeater_ctl" + projectIndex +"_ProjectInformatie1_ProjectNaamLabel").first().text();
+      var htmlNode = $(projectNode);
+
+      var projectName = htmlNode.find("#ctl00_ctl00_ctl00_ContentPlaceHolderDefault_MainContentPlaceHolder_ctl00_ProjectenFilter_4_Projectgegevens1_LeningRepeater_ctl" + projectIndex + "_ProjectInformatie1_ProjectNaamLabel").first().text();
+      var loanGoal = htmlNode.find("#ctl00_ctl00_ctl00_ContentPlaceHolderDefault_MainContentPlaceHolder_ctl00_ProjectenFilter_4_Projectgegevens1_LeningRepeater_ctl" + projectIndex + "_ProjectInformatie1_LeendoelLabel").first().text();
+
+      debugger;
       return {
-        projectName : projectName
+        projectName : projectName,
+        loanGoal : loanGoal
       };
     });
 
